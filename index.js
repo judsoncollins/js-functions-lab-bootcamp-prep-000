80873afb6242da17aa6dc690a16c9bc128bc59cf
Judson Collins <jcollins@jdanet.net>
2017-10-18 16:59:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // write your code below!
+function happyHolidays()  {
+  return "Happy holidays!"
+}
+
+
+var name = Judson
+
+function happyHolidaysTo(name)  {
+  return `Happy holidays, ${name}!`
+}
+
+var days = 3
+var holiday = SatanicWorship
+
+function happyHolidayTo(days, holiday)  {
+  return `It's ${days} days until ${holiday}`
+}
